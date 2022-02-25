44cf87862148eda7c2bb9aeb29bca35fe15b0f60
khadirov <bqodirov474@gmail.com>
2022-02-25 19:23:31 +0500

    Add package json file

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      'sm': '576px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '992px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1200px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1400px',
+      // => @media (min-width: 1536px) { ... }
+    },
+
+    // spacing: {},
+
+    colors: {
+      'primary': '#FFD2A4',
+      'header-bg': '#0D0D0D',
+      'white': '#fff',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
